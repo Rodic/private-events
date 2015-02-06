fc6bf3957883c678c4357d55b8a6b9d45bf0c2be
rodic <alec.rodic@gmail.com>
2015-02-06 18:55:34 +0100

    Tests fixed

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_options = {from: 'no-reply@private-events.com'}
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
--- spec/mailers/users_mailer_spec.rb
+++ spec/mailers/users_mailer_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 
 RSpec.describe UsersMailer, :type => :mailer do
   describe "thank_you" do
-    let(:mail) { UsersMailer.thank_you }
+    let(:user) { FactoryGirl.create(:user) }
+    let(:mail) { UsersMailer.thank_you(user) }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Thank you")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["no-reply@private-events.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match(/Thank you for choosing us/)
     end
   end
-
 end
